a6d852d100a504fa8bb58a4e3e122be81dca9a3c
Saptak Dutta <161841612+Saptak-d@users.noreply.github.com>
2024-08-03 17:16:17 +0530

    Add files via upload

---
+++ project_4_currency converter/app.js
@@ -0,0 +1,62 @@
+ const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+ 
+  const dropdown = document.querySelectorAll(".dropdown select");
+  const btn = document.querySelector("form button");
+  const fromCurr = document.querySelector(".from select");
+  const toCurr = document.querySelector(".to select");
+  const msg = document.querySelector(".msg");
+ 
+  for(let select of dropdown){
+    for(currCode in countryList){
+     let newOption  = document.createElement("option");
+     newOption.innerText = currCode;
+     newOption.value = currCode 
+     if(select.name === "from" && currCode === "USD"){
+        newOption.selected = "selected";
+     }
+     else if(select.name === "to" && currCode === "INR"){
+        newOption.selected = "selected";
+     }
+     select.append(newOption);
+      }
+      select.addEventListener("change", (evt) => {
+        updataFlag(evt.target);
+      });
+  }
+  updataExchagerate = async() => {
+    let amount = document.querySelector(".amount  input");
+    let amtval = amount.value;
+    console.log(amtval);
+    if(amtval === "" || amtval < 1){
+        amtval=1;
+        amount.value ="1";
+    }
+   const URL =  `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  
+  let response = await fetch(URL);
+  let data = await response.json();
+  
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()]; 
+  
+  let finalAnount = amtval*rate;
+  msg.innerText = `${amtval}${fromCurr.value} = ${finalAnount} ${toCurr.value}`
+ 
+  };
+  const updataFlag = (element) => {
+   let currCode = element.value;
+   let countryCode = countryList[currCode];
+   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+   let img  = element.parentElement.querySelector("img");
+   img.src = newSrc;
+
+  };
+  btn.addEventListener("click" , (evt) => {
+    evt.preventDefault();
+    updataExchagerate();
+    
+  });
+  window.addEventListener("load",() =>{
+    updataExchagerate();
+});
+
+  
